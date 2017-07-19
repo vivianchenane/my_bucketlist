289a4274f5a48590040d39171620b2ea72cac644
vee <vivianchenane@gmail.com>
2017-07-19 09:42:14 +0300

    created unit tests for creating a category

--- test_units.py
+++ test_units.py
@@ -9,23 +9,44 @@ bucket_items = []
 
 def createItem(name,id,createdBy, category):
  	item = BucketlistItem()
- 	item.name=name
- 	item.id=id
- 	item.createdBy=createdBy
- 	item.category=category
+ 	item.name = name
+ 	item.id = id
+ 	item.createdBy = createdBy
+ 	item.category = category
 
  	bucket_items.append(item)
  	return item
 
-def test_vee():
-	assert createItem('curtis',1, 'vivian','entertainment').name == 'curtis'
-
+def test_createItem():
+	assert createItem('curtis', 1, 'vivian','entertainment').name == 'curtis'
 
 def test_createItem1():
-	assert createItem('curtis',1, 'vivian','entertainment').name == 'cu'
+	assert createItem('curtis', 1, 'vivian','entertainment').name == 'cu'
 
 def test_createItem2():
-	assert createItem('curtis',1, 'vivian','entertainment').id == 1
+	assert createItem('curtis', 1, 'vivian','entertainment').id == 1
+
+category = []
+
+def createCategory(name,id,createdBy):
+    categories = Category()
+    categories.name = name
+    categories.id = id
+    categories.createdBy = createdBy
+
+    category.append(categories)
+    return categories
+
+def test_createCategory():
+    assert createCategory('movies', 1, 'vivian').id == 1
+
+def test_createCategory1():
+    assert createCategory1('movies', 1, 'vivian').category == 'series'
+
+
+
+
+
 
 
     
