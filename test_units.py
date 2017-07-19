de621d8e2534487a3604c5fde89ea70b23661c82
vee <vivianchenane@gmail.com>
2017-07-19 13:35:50 +0300

    created unit tests for removing an item from a list

--- test_units.py
+++ test_units.py
@@ -17,14 +17,31 @@ def createItem(name,id,createdBy, category):
  	bucket_items.append(item)
  	return item
 
+
 def test_createItem():
-	assert createItem('curtis', 1, 'vivian','entertainment').name == 'curtis'
+	assert createItem('music', 1, 'vivian','entertainment').name == 'music'
+
+def test_checkItemListSize():
+	assert len(bucket_items) == 1
 
 def test_createItem1():
-	assert createItem('curtis', 1, 'vivian','entertainment').name == 'cu'
+	assert createItem('music', 2, 'vivian','travel').name == 'cu'
+
+def test_checkItemListSize():
+	assert len(bucket_items) == 2
 
 def test_createItem2():
-	assert createItem('curtis', 1, 'vivian','entertainment').id == 1
+	assert createItem('music', 3, 'vivian','books').id == 1
+
+def test_checkItemListSize():
+	assert len(bucket_items) == 3
+
+
+def test_deleteItem():
+   del bucket_items[0]
+   assert len(bucket_items) == 2
+
+
 
 category = []
 
@@ -41,7 +58,13 @@ def test_createCategory():
     assert createCategory('movies', 1, 'vivian').id == 1
 
 def test_createCategory1():
-    assert createCategory1('movies', 1, 'vivian').category == 'series'
+    assert createCategory('books', 2, 'vivian').name == 'series'
+
+def test_createCategory3():
+    assert createCategory('Entertainment', 3, 'vivian').createdBy == 'nelly'
+
+
+
 
 
 
