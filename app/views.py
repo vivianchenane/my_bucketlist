84986442d3777bea2789a5878d42567d63f46883
vee <vivianchenane@gmail.com>
2017-07-19 23:50:10 +0300

    implemented template inheritance

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from flask import render_template, flash, redirect
 from app import app
-from .forms import LoginForm
 
 
 @app.route('/')
@@ -8,21 +7,19 @@ from .forms import LoginForm
 def index():
     return render_template('index.html', title='Sign In')
 
-
 @app.route('/register')
 def register():
     return render_template('register.html', title='Register')
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
     return render_template('dashboard.html', title='Dashboard')
-
 
 @app.route('/logout')
 def logout():
     return redirect('/index')
 
-@app.route('/add_item')
+@app.route('/additem')
 def addItem():
    return render_template('add_item.html', title='Add Item')
 
@@ -34,4 +31,5 @@ def addcategory():
 @app.route('/categorylist')
 def categorylist():
     return render_template('category_list.html', title='Category List')
+
 
