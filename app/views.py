24655e1f8bf9d8f4298e55c7693d07ad2593f991
vivianchenane <vivianchenane@gmail.com>
2017-07-21 10:49:55 +0300

    FEATURE: save bucket list item on form submit

--- app/views.py
+++ app/views.py
@@ -1,8 +1,20 @@
 from flask import render_template, flash, redirect, session, request
 from app import app
+from .models import BucketListItem, Category
 
 
 database_users = {'username': 'user' ,'password': 'user'}
+
+bucket_list_items = [
+    {
+        'id': '1',
+        'name': 'Travel to coast via SGR',
+        'description': 'Going to Mombasa',
+        'category': 'Travel',
+        'user': 'vivian',
+        'date': '30/07/2017'
+    }
+]
 
 
 @app.route('/')
@@ -36,15 +48,28 @@ def login():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html', title='Dashboard')
+    return render_template('dashboard.html', title='Dashboard',bucket_list_items=bucket_list_items)
 
 @app.route('/logout') 
 def logout():
     return redirect('/index')
 
-@app.route('/additem' ,methods = ['GET' , 'POST'])
-def addItem():
-   return render_template('add_item.html', title='Add Item')
+
+@app.route('/add_item',methods= ['GET','POST'])
+def add_item():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        description = request.form.get('description')
+        category = request.form.get('category')
+        user = session['username']
+        date = request.form.get('date')
+        id = len(bucket_list_items) + 1
+
+        bucket_item = BucketListItem(id,name,description,category,user,date)
+        bucket_list_items.append(bucket_item)
+        return redirect('/dashboard')
+
+    return render_template('add_item.html', title='Add Item')
 
 
 @app.route('/addcategory')
